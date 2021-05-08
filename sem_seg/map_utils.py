d687b914b98dfce46bc8e10fb5f8d6c27e9bca2e
mmabadal <34652305+mmabadal@users.noreply.github.com>
2021-05-09 01:12:37 +0200

    Update map_utils.py

--- sem_seg/map_utils.py
+++ sem_seg/map_utils.py
@@ -26,5 +26,36 @@ def get_info_map(info_map, info_world):
 def clean_map(info_map, count_thr):
 
 
+    # for instance i
+        # if count i < count_thr
+            # mark instance i to delete
+    # delete instances
+    # ESTO SE PUEDE HACER POR DENSIDAD???
+
+
+    # get_info instances pipe
+    # RECALCULATE BELONGING INST OF PIPES (old ones and newly gotten)
+    # RECALCULATE NEAR PIPES OF CONNEXIONS (old ones and newly gotten)
+
+    #for valve
+        #if count < count_thr
+            #delete valve
+    # if list deleted instances len > 0
+        # RECALCULATE NEAR PIPES OF VALVES (old ones and newly gotten)
+
 
     return info_map
+
+
+    ############# OPCION 2 #############
+    # for pipe p
+        # if all belonging instances are on list of instances deleted
+            # mark pipe to delete
+    # delete pipes
+
+    # RECALCULATE BELONGING INST OF PIPES (old ones and newly gotten)
+
+    #for connexion c
+        # delete near pipes that are in pipes deleted
+    # refine connexions?
+    ####################################
