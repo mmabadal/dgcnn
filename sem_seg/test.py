3afef89037312eaf8827a376ab16779d1461fba7
mmabadal <34652305+mmabadal@users.noreply.github.com>
2023-06-26 10:38:03 +0200

    Update test.py

--- sem_seg/test.py
+++ sem_seg/test.py
@@ -9,7 +9,17 @@ def quaternion_multiply(q0, q1):
     x1, y1, z1, w1 = q1
     return np.array([-x1*x0-y1*y0-z1*z0+w1*w0, x1*w0+y1*z0-z1*y0+w1*x0, -x1*z0+y1*w0+z1*x0+w1*y0, x1*y0-y1*x0+z1*w0+w1*z0], dtype=np.float64)
 
+    
+def get_tr(t, q):
+    trans = np.array([[1, 0, 0, t[0]], [0, 1, 0, t[1]], [0, 0, 1, t[2]], [0, 0, 0, 1]], np.float)
+    rot = Rot.from_quat(q)
+    rot_mat = rot.as_matrix()
+    trans_rot = copy.deepcopy(trans)
+    trans_rot[0:3, 0:3] = rot_mat
+    return(trans_rot)
 
+
+    
 if __name__=='__main__':
 
     # with open("graph.txt", "r") as file:
@@ -36,26 +46,42 @@ if __name__=='__main__':
     tq_down_left = np.array([-0.06, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
     t_down_left = tq_down_left[:3]
     q_down_left = tq_down_left[3:]
+
+    tr_ned_baselink = get_tr(t_ned_baselink, q_ned_baselink)
+    tr_baselink_stick = get_tr(t_baselink_stick, q_baselink_stick)
+    tr_stick_downbase = get_tr(t_stick_downbase, q_stick_downbase)
+    tr_downbase_down  = get_tr(t_downbase_down, q_downbase_down)
+    tr_down_left = get_tr(t_down_left, q_down_left)
+
+
+    np.matmul(a, b)
+
+    tr_ned_stick = np.matmul(tr_ned_baselink, tr_baselink_stick)
+    tr_ned_downbase = np.matmul(tr_ned_stick, tr_stick_downbase)
+    tr_ned_down = np.matmul(tr_ned_downbase, tr_downbase_down)
+    tr_ned_left = np.matmul(tr_ned_down, tr_down_left)
+
+    print(tr_ned_left)
+
+
+    print("-------------------")
+    print("-------------------")
+
     t_ned_left = t_ned_baselink + t_baselink_stick + t_stick_downbase + t_downbase_down + t_down_left
 
     q_ned_stick = quaternion_multiply(q_ned_baselink, q_baselink_stick)
     q_ned_downbase = quaternion_multiply(q_ned_stick, q_stick_downbase)
     q_ned_down = quaternion_multiply(q_ned_downbase, q_downbase_down)
     q_ned_left = quaternion_multiply(q_ned_down, q_down_left)
-    
-    T = np.array([[1, 0, 0, t_ned_left[0]], [0, 1, 0, t_ned_left[1]], [0, 0, 1, t_ned_left[2]], [0, 0, 0, 1]], np.float)
-    R = Rot.from_quat(q_ned_left)
-    Rm = R.as_matrix()
-    
-    TR = copy.deepcopy(T)
-    TR[0:3, 0:3] = Rm
 
-    print(T)
-    print(Rm)
-    print(TR)
+    tr_down_left = get_tr(t_ned_left, q_ned_left)
     
-    
-    
+    print(tr_down_left)
+
+    print("-------------------")
+    print("-------------------")
+
+
 
 
 
