54327dc60f6c116fd00a60582cee900e232f1a4b
mmabadal <34652305+mmabadal@users.noreply.github.com>
2023-07-05 09:59:59 +0200

    Update inference_ros_tf_odom.py

--- sem_seg/inference_ros_tf_odom.py
+++ sem_seg/inference_ros_tf_odom.py
@@ -153,7 +153,7 @@ class Pointcloud_Seg:
         print("loop is: " + str(self.loop))
         if loop.data != self.loop:
             self.loop = loop.data
-            self.update_positions(path_pc, self.path_graph)
+            self.update_positions()
 
 
     def set_model(self):
@@ -294,7 +294,7 @@ class Pointcloud_Seg:
             vector = vector*0.18                                                             # resize vector to valve size //PARAM
             info_valves_list.append([xyz_central, vector, max_idx, inst[:,0:3], max_info])   # append valve instance info
 
-        info_valves_list.append([np.array([0.2,0.2,0.2]),np.array([0.1,0.1,0])],1,np.array([[1,2,3], [4,5,6]]),0.5) # BORRAAAAARR, ESTO ES PARA TEST!!!!!!!
+        info_valves_list.append([np.array([0.2,0.2,0.2]),np.array([0.1,0.1,0]),1,np.array([[1,2,3], [4,5,6]]),0.5]) # BORRAAAAARR, ESTO ES PARA TEST!!!!!!!
 
             # print best valve matching
             #trans = np.eye(4) 
