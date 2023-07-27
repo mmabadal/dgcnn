7174ba123bf20ab407723be392db24746653239e
bomiquel <bomiquel@gmail.com>
2023-07-27 13:19:45 +0200

    Test polygon ok

--- sem_seg/test.py
+++ sem_seg/test.py
@@ -23,32 +23,34 @@ def set_margin(points, center, margin):
  
 if __name__=='__main__':
 
-    # info = np.load("a/b/c")
+    info = np.load("/home/bomiquel/SLAM_ws/src/dgcnn/test_polygon/out/1604421321894689_info_ref.npy", allow_pickle = True)
 
-    # info_pipes_list = info[0]
-    # info_connexions_list = info[1]
-    # info_valves_list = info[2]
-    # instances_ref_pipe_list = info[3]
+    info_pipes_list = info[0]
+    info_connexions_list = info[1]
+    info_valves_list = info[2]
+    instances_ref_pipe_list = info[3]
     
     # info3 = [info_pipes_list2, info_connexions_list2, info_valves_list2, instances_ref_pipe_list]
     # info_pipes_list = info3[0]
     # info_valves_list = info3[2]
 
-    info_pipes_list = list()
-    info_pipes_list.append([np.array([[1,2,3], [4,5,6], [4,5,6], [4,5,6], [4,5,6]]),[np.array([[1,2,3]])], [np.array([[3,3,3], [4,5,6]])]])
+    # info_pipes_list = list()
+    # info_pipes_list.append([np.array([[1,2,3], [4,5,6], [4,5,6], [4,5,6], [4,5,6]]),[np.array([[1,2,3]])], [np.array([[3,3,3], [4,5,6]])]])
 
-    info_valves_list = list()
-    info_valves_list.append([np.array([0.2,0.2,0.2]),np.array([[1,2,3]]),1,np.array([[1,2,3], [4,5,6]]), np.array([0.5])])
+    # info_valves_list = list()
+    # info_valves_list.append([np.array([0.2,0.2,0.2]),np.array([[1,2,3]]),1,np.array([[1,2,3], [4,5,6]]), np.array([0.5])])
 
     points_list = list()
 
     margin = 0.05
 
+    print(info_pipes_list)
+
     for pipe_info in info_pipes_list:
 
         chain = pipe_info[0]
-        elbow_list = pipe_info[1][0]
-        vector_list = pipe_info[2][0]
+        elbow_list = pipe_info[1]
+        vector_list = pipe_info[2]
 
         vector = vector_list[0][0:2]
 
@@ -103,7 +105,7 @@ if __name__=='__main__':
     for valve_info in info_valves_list:
 
         center = valve_info[0][0:2]
-        vector = valve_info[1][0][0:2]
+        vector = valve_info[1][0:2]
         vector_orth = np.array([-vector[1], vector[0]])
         vector_orth = vector_orth/np.linalg.norm(vector_orth)
         vector_orth = 0.09 * vector_orth
