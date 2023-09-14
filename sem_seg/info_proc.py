6aea6fb7f334223e0a06e276ef999c3f41bdc397
mmabadal <34652305+mmabadal@users.noreply.github.com>
2023-09-14 13:36:39 +0200

    final test margins

--- sem_seg/info_proc.py
+++ sem_seg/info_proc.py
@@ -8,35 +8,6 @@ from sensor_msgs.msg import CameraInfo
 from geometry_msgs.msg import Point32, Polygon
 
 
-def set_margin(points, point1, point2, margin):
-
-    for i, point in enumerate(points):
-        if i<2:
-            if point[0] >= point1[0]:
-                point[0] += margin
-            else:
-                point[0] -= margin
-            if point[1] >= point1[1]:
-                point[1] += margin
-            else:
-                point[1] -= margin
-            points[i] = point
-
-    for i, point in enumerate(points):
-        if i>1:
-            if point[0] >= point2[0]:
-                point[0] += margin
-            else:
-                point[0] -= margin
-            if point[1] >= point2[1]:
-                point[1] += margin
-            else:
-                point[1] -= margin
-            points[i] = point
-
-    return points
-
-
 def points_to_img(points_list, id, c_info, path):
 
     decimation = c_info.binning_x
@@ -54,24 +25,18 @@ def points_to_img(points_list, id, c_info, path):
      #   0    1988   714   *     y    =   v  =  1988*y + 714*z 
      #   0     0      1          z        w  =  z 
 
-    points_list2 = list()
+    points_list_2d = list()
 
-    keyframes = os.listdir(path)
-    for keyframe in keyframes:
-        if "left" in keyframe:
-            if id in keyframe:
-                key = Image.open(os.path.join(path, keyframe))
-                break
-    k = -1
+    # keyframes = os.listdir(path)
+    # for keyframe in keyframes:
+    #     if "left" in keyframe:
+    #         if id in keyframe:
+    #             key = Image.open(os.path.join(path, keyframe))
+    #             break
 
     for points in points_list:
 
         points2 = list()
-
-        k = k+1
-        if k == 4:
-            k = 0
-        colors = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255, 255),(255,255,255),(128,0,0),(0,128,0),(0,0,128),(128,128,0),(128,0,128)]
 
         for point in points:
 
@@ -92,16 +57,16 @@ def points_to_img(points_list, id, c_info, path):
             xdisp = int(np.clip(xdisp, 1, width-1))
             ydisp = int(np.clip(ydisp, 1, height-1))
 
-            key.putpixel((xdisp, ydisp), colors[k])
+            # key.putpixel((xdisp, ydisp), (255,0,0))
 
             xydisp = np.array((ydisp, xdisp))
             points2.append(xydisp)
 
-        points_list2.append(points2)
+        points_list_2d.append(points2)
 
-    key.save("/home/bomiquel/Desktop/" + str(id) + "_colour.png")
+    # key.save("/home/bomiquel/Desktop/" + str(id) + "_colour.png")
 
-    return points_list2
+    return points_list_2d
 
 def get_bb(info, margin, id, c_info, path):
 
@@ -126,81 +91,48 @@ def get_bb(info, margin, id, c_info, path):
         vector = vector_list[0][0:3]
 
         point1 = chain[0][0:3]
-        point2 = point1 + vector
-
         center = point1 + vector/2
 
         vector_orth = np.array([-vector[1], vector[0], 0])
         vector_orth = vector_orth/np.linalg.norm(vector_orth)
-        vector_orth = 0.06 * vector_orth
+        vector_orth = (0.06+margin) * vector_orth
 
-        point3 = point1 + vector_orth/2
-        point4 = point1 - vector_orth/2
-        point5 = point2 + vector_orth/2
-        point6 = point2 - vector_orth/2
-
-        points1 = [point1, point1, point2, point2]
-        points_list.append(points1)
-        points2 = [point3, point4, point5, point6]
-        points_list.append(points2)
-        #points22 = copy.deepcopy(points2)
-        #points3 = set_margin(points22, point1, point2, margin)
         vector_margin = vector_list[0][0:3]*1.1
         vector_margin2 = vector_margin/np.linalg.norm(vector_margin)
-        vector_margin2 = 0.08 * vector_margin2
+        vector_margin2 = margin*2 * vector_margin2
         vector_margin = vector_margin +vector_margin2
         point1_margin = center - (vector_margin/2)
         point2_margin = center + (vector_margin/2)
-        point3_margin = point1_margin + ((vector_orth/2)*(5/3))
-        point4_margin = point1_margin - ((vector_orth/2)*(5/3))
-        point5_margin = point2_margin + ((vector_orth/2)*(5/3))
-        point6_margin = point2_margin - ((vector_orth/2)*(5/3))
+        point3_margin = point1_margin + ((vector_orth/2))
+        point4_margin = point1_margin - ((vector_orth/2))
+        point5_margin = point2_margin + ((vector_orth/2))
+        point6_margin = point2_margin - ((vector_orth/2))
         points3 = [point3_margin, point4_margin, point5_margin, point6_margin]
         points_list.append(points3)
 
-        print("after pipe")
-        print(points_list)
-
         for i, elbow in enumerate(elbow_list):
-
-            point1 = elbow[0:3]
+            
             vector = vector_list[i+1][0:3]
 
-            point2 = point1 + vector
-
+            point1 = elbow[0:3]
             center = point1 + vector/2
 
             vector_orth = np.array([-vector[1], vector[0],0])
             vector_orth = vector_orth/np.linalg.norm(vector_orth)
-            vector_orth = 0.06 * vector_orth
+            vector_orth = (0.06+margin) * vector_orth
 
-            point3 = point1 + vector_orth/2
-            point4 = point1 - vector_orth/2
-            point5 = point2 + vector_orth/2
-            point6 = point2 - vector_orth/2
-
-            points1 = [point1, point1, point2, point2]
-            points_list.append(points1)
-            points2 = [point3, point4, point5, point6]
-            points_list.append(points2)
-            #points22 = copy.deepcopy(points2)
-            #points3 = set_margin(points22, point1, point2, margin)
             vector_margin = vector_list[0][0:3]*1.1
             vector_margin2 = vector_margin/np.linalg.norm(vector_margin)
-            vector_margin2 = 0.08 * vector_margin2
+            vector_margin2 = margin*2 * vector_margin2
             vector_margin = vector_margin +vector_margin2
             point1_margin = center - (vector_margin/2)
             point2_margin = center + (vector_margin/2)
-            point3_margin = point1_margin + ((vector_orth/2)*(5/3))
-            point4_margin = point1_margin - ((vector_orth/2)*(5/3))
-            point5_margin = point2_margin + ((vector_orth/2)*(5/3))
-            point6_margin = point2_margin - ((vector_orth/2)*(5/3))
+            point3_margin = point1_margin + ((vector_orth/2))
+            point4_margin = point1_margin - ((vector_orth/2))
+            point5_margin = point2_margin + ((vector_orth/2))
+            point6_margin = point2_margin - ((vector_orth/2))
             points3 = [point3_margin, point4_margin, point5_margin, point6_margin]
             points_list.append(points3)
-
-            print("after elbow")
-            print(points_list)
-
 
     for valve_info in info_valves_list:
 
@@ -208,49 +140,27 @@ def get_bb(info, margin, id, c_info, path):
         vector = valve_info[1][0:3]
         
         point1 = center - (vector/2)
-        point2 = center + (vector/2)
 
         vector_orth = np.array([-vector[1], vector[0],0])
         vector_orth = vector_orth/np.linalg.norm(vector_orth)
-        vector_orth = 0.08 * vector_orth
+        vector_orth = (0.08+margin) * vector_orth
 
-        point3 = point1 + vector_orth/2
-        point4 = point1 - vector_orth/2
-        point5 = point2 + vector_orth/2
-        point6 = point2 - vector_orth/2
-
-        points1 = [point1, point1, point2, point2]
-        points_list.append(points1)
-        points2 = [point3, point4, point5, point6]
-        points_list.append(points2)
-        #points22 = copy.deepcopy(points2)
-        #points3 = set_margin(points22, point1, point2, margin)
         vector_margin = vector_list[0][0:3]*1.1
         vector_margin2 = vector_margin/np.linalg.norm(vector_margin)
-        vector_margin2 = 0.08 * vector_margin2
+        vector_margin2 = margin*2 * vector_margin2
         vector_margin = vector_margin +vector_margin2
         point1_margin = center - (vector_margin/2)
         point2_margin = center + (vector_margin/2)
-        point3_margin = point1_margin + ((vector_orth/2)*1.5)
-        point4_margin = point1_margin - ((vector_orth/2)*1.5)
-        point5_margin = point2_margin + ((vector_orth/2)*1.5)
-        point6_margin = point2_margin - ((vector_orth/2)*1.5)
+        point3_margin = point1_margin + ((vector_orth/2))
+        point4_margin = point1_margin - ((vector_orth/2))
+        point5_margin = point2_margin + ((vector_orth/2))
+        point6_margin = point2_margin - ((vector_orth/2))
         points3 = [point3_margin, point4_margin, point5_margin, point6_margin]
         points_list.append(points3)
-
-        print("after valve")
-        print(points_list)
-
-    print("final 3d")
-    print(points_list)
     
-    points_list_2 = points_to_img(points_list, id, c_info, path)
+    points_list_2d = points_to_img(points_list, id, c_info, path)
 
-
-    print("final 2d")
-    print(points_list_2)
-
-    for i, points in enumerate(points_list_2):
+    for i, points in enumerate(points_list_2d):
 
         p1.x = points[0][0]
         p1.y = points[0][1]
