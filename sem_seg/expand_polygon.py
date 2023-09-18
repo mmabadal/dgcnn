aeca99616d7469c26c5b504db35052fbd08a16d6
mmabadal <34652305+mmabadal@users.noreply.github.com>
2023-09-18 14:21:49 +0200

    Update expand_polygon.py

--- sem_seg/expand_polygon.py
+++ sem_seg/expand_polygon.py
@@ -3,122 +3,112 @@
 
 
 import numpy as np
-from skimage import io
+from skimage import io, color
 
 
-
-im = io.imread("/home/miguel/Desktop/test_polygon/left.jpeg")
-box_list = list()
+def main():
 
 
-p1 = np.array([448, 26 ])
-p2 = np.array([472, 139])
-p3 = np.array([249, 49 ])
-p4 = np.array([269, 168])
-p5 = np.array([253, 116])
-p6 = np.array([468, 83 ])
-box = (p1, p2, p3, p4, p5, p6)
-box_list.append(box)
+    box_list = list()
 
-p1 = np.array([246, 81 ])
-p2 = np.array([258, 142])
-p3 = np.array([137, 101])
-p4 = np.array([151, 155])
-p5 = np.array([252, 110])
-p6 = np.array([147, 129])
-box = (p1, p2, p3, p4, p5, p6)
-box_list.append(box)
+    img = io.imread("/home/miguel/Desktop/test_polygon/left.jpeg")
+    imshape = img.shape
 
-p1 = np.array([630, 445])
-p2 = np.array([591, 505])
-p3 = np.array([713, 504])
-p4 = np.array([674, 569])
-p5 = np.array([612, 477])
-p6 = np.array([693, 535])
-box = (p1, p2, p3, p4, p5, p6)
-box_list.append(box)
+    margin = 25
+    dist = 10
+    cthr = 0
+    nthr = 15
+    vstride = 4
+    maxmins = np.array([400,600,120,100])
 
 
-for box in box_list:
-    for i, point in enumerate(box):
-        im[point[0], point[1], 0] = 0
-        im[point[0], point[1], 1] = 255
-        im[point[0], point[1], 2] = 0
-        if i > 3:
-            im[point[0], point[1], 0] = 255
-            im[point[0], point[1], 1] = 0
-            im[point[0], point[1], 2] = 0
+    p1 = np.array([448, 26 ])
+    p2 = np.array([472, 139])
+    p3 = np.array([249, 49 ])
+    p4 = np.array([269, 168])
+    p5 = np.array([253, 116])
+    p6 = np.array([468, 83 ])
+    box = (p1, p2, p3, p4, p5, p6)
+    box_list.append(box)
+
+    p1 = np.array([246, 81 ])
+    p2 = np.array([258, 142])
+    p3 = np.array([137, 101])
+    p4 = np.array([151, 155])
+    p5 = np.array([252, 110])
+    p6 = np.array([147, 129])
+    box = (p1, p2, p3, p4, p5, p6)
+    box_list.append(box)
+
+    p1 = np.array([630, 445])
+    p2 = np.array([591, 505])
+    p3 = np.array([713, 504])
+    p4 = np.array([674, 569])
+    p5 = np.array([612, 477])
+    p6 = np.array([693, 535])
+    box = (p1, p2, p3, p4, p5, p6)
+    box_list.append(box)
 
 
+    for box in box_list:
+        for i, point in enumerate(box):
+            img[point[0], point[1], 0] = 0
+            img[point[0], point[1], 1] = 255
+            img[point[0], point[1], 2] = 0
+            if i > 3:
+                img[point[0], point[1], 0] = 255
+                img[point[0], point[1], 1] = 0
+                img[point[0], point[1], 2] = 0
 
 
-for box in box_list:
+    for box in box_list:
 
-    border = check_box(box, imshape, margin) # TODO en realidad se le tendra que pasar un xmax xmin ymax ymin i ver si estan cerca del borde de la disparidad no de la imagen, primera box de la lista que sea
-    if border = False:                       # las 4 esquinas? o como lo pasa bo de disp a imagen?
-        next()
+        border = check_box(box, maxmins, margin) # TODO en realidad se le tendra que pasar un xmax xmin ymax ymin i ver si estan cerca del borde de la disparidad no de la imagen, primera box de la lista que sea
+        if border == False:                       # las 4 esquinas? o como lo pasa bo de disp a imagen?
+            next()
 
-    point_check_list = list()
+        vector45 = box[5]-box[4]
+        vector45_unit = vector45/np.linalg.norm(vector45)
+        vector54_unit = vector45_unit*-1
 
-    vector45 = box[5]-box[4]
-    vector45_unit = vector45/np.linalg.norm(vector45)
-    vector54_unit = vector45_unit*-1
-
-    vector45_iter = vector45_unit * 4
-    vector54_iter = vector54_unit * 4
+        vector45_iter = vector45_unit * vstride
+        vector54_iter = vector54_unit * vstride
 
 
-    iter = 0
-    p_list = list()
-    while 1:
-        iter += 1
-        point = int(box[5] + iter * vector45_iter)
-        p_list.append(point)
-        if point[0] < 0 or point[0] > imshape[0] or point[1] < 0 or point[1] > imshape[1]: # TODO se le puede poner aqui el margen del buscador manhattan y nos ahorrariamos tener que considerar out of range en éste
-            p_end1 = p_list[-2]
-            break
-        else:
-            end = check_near(point, imshape)
-            if end == True:
-                p_end1 = p_list[-1]  # TODO -1 + (- dist manhatan de check near/4) - para tirar para atras los puntos que añadirá de mas al ir encontrando tuberia por atras (/4 pq vamos a saltos de 4 en 4, check vector_iter)
+        iter = 0
+        p_list = list()
+        while 1:
+            iter += 1
+            point = int(box[5] + iter * vector45_iter)
+            p_list.append(point)
+            if point[0] < 0 or point[0] > imshape[0] or point[1] < 0 or point[1] > imshape[1]: # TODO se le puede poner aqui el margen del buscador manhattan y nos ahorrariamos tener que considerar out of range en éste
+                p_end1 = p_list[-2] # el ultimo que tuvo tuberia antes de salirse
                 break
+            else:
+                end = check_near(point, dist, img, cthr, nthr)
+                if end == True:
+                    a = -1 - dist/vstride #  -1 - dist/vstride para tirar para atras los puntos que añadirá de mas al ir encontrando tuberia por atras (/vstride pq vamos a saltos de vstride pixeles)
+                    p_end1 = p_list[a]  
+                    break
 
-    iter = 0
-    p_list = list()
-    while 1:
-        iter += 1
-        point = int(box[4] + iter * vector54_iter)
-        p_list.append(point)
-        if point[0] < 0 or point[0] > imshape[0] or point[1] < 0 or point[1] > imshape[1]: # TODO se le puede poner aqui el margen del buscador manhattan y nos ahorrariamos tener que considerar out of range en éste
-            p_end2 = p_list[-2]
-            break
-        else:
-            end = check_near(point, imshape)
-            if end == True:
-                p_end2 = p_list[-1]  # TODO -1 + (- dist manhatan de check near/4) - para tirar para atras los puntos que añadirá de mas al ir encontrando tuberia por atras (/4 pq vamos a saltos de 4 en 4, check vector_iter)
+        iter = 0
+        p_list = list()
+        while 1:
+            iter += 1
+            point = int(box[4] + iter * vector54_iter)
+            p_list.append(point)
+            if point[0] < 0 or point[0] > imshape[0] or point[1] < 0 or point[1] > imshape[1]: # TODO se le puede poner aqui el margen del buscador manhattan y nos ahorrariamos tener que considerar out of range en éste
+                p_end2 = p_list[-2] # el ultimo que tuvo tuberia antes de salirse
                 break
+            else:
+                end = check_near(point, dist, img, cthr, nthr)
+                if end == True:
+                    a = -1 - dist/vstride #  -1 - dist/vstride para tirar para atras los puntos que añadirá de mas al ir encontrando tuberia por atras (/vstride pq vamos a saltos de vstride pixeles)
+                    p_end2 = p_list[a]  
+                    break
 
-
-    
-
-def check_box(box, imshape, margin):
-    border = False
-    for point in box:
-        if point[0] < margin or point[0] > imshape[0]-margin or point[1] < margin or point[1] > imshape[1]-margin:
-            border = True
-            break
-    return border
-
-
-def check_near(point, imshape)
-
-    end = True
-
-    # TODO for por los ixj pixeles de alrededor (distancia manhatan) y si alguno forma aprte de tuberia (por color), end = False y break
-
-    return end
-
-
+    io.imshow(img)
+    io.show()
 
 # añadir nuevas iteraciones para vectores ortogonales que se haran a partir de los puntos end y que sera +- vector unitario ortogonal a los vectores unit o directamente iter y 
 # sera en loop hasta que no se encuentren near, tener en cuuenta que no se vaya a infinito ya que al principio detectara la propia tuberia, se puede anular el principio, hacer
@@ -127,30 +117,46 @@ def check_near(point, imshape)
 
 
 
+def check_box(box, maxmins, margin):
+    border = False
+
+    maxx, maxy, minx, miny = maxmins
+
+    for point in box:
+        if (point[0] < minx+margin) or (point[0] > maxx[0]-margin) or (point[1] < miny+margin) or (point[1] > maxy[1]-margin):
+            border = True
+            break
+    return border
+
+
+def check_near(point, dist, img, cthr, nthr):
+
+    color_ref = np.array([210,210,0])
+    color_ref_lab = color.rgb2lab([[[color_ref[0] / 255, color_ref[1] / 255, color_ref[2] / 255]]])
+    end = True 
+    imshape = img.shape
+
+    row0 = max(point[0]-dist,0)
+    row1 = min(point[0]+dist+1, imshape[0])
+    col0 = max(point[1]-dist,0)
+    col1 = min(point[1]+dist+1, imshape[1])
+
+    n = 0
+
+    for row in range(row0, row1):                               # for each row
+        for col in range(col0, col1):                           # for each col
+            pixel = np.array([img[row,col,0],img[row,col,1],img[row,col,2]])
+            pixel_lab = color.rgb2lab([[[pixel[0] / 255, pixel[1] / 255, pixel[2] / 255]]])
+            color_dist = color.deltaE_cie76(color_ref_lab, pixel_lab, channel_axis=-1)
+            if color_dist < cthr:
+                n += 1 
+    if n > nthr:
+        end = False
+    return end
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-io.imshow(im)
-io.show()
-
-
-
-
-
+if __name__ == "__main__":
+    main()
 
