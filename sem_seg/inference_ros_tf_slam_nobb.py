08bb1deafd276cc8e9ce3f8e367dc46b2237eb86
bomiquel <bomiquel@gmail.com>
2025-06-30 11:25:55 +0200

    Updated inference_ros_tf_slam_nobb.py.

--- sem_seg/inference_ros_tf_slam_nobb.py
+++ sem_seg/inference_ros_tf_slam_nobb.py
@@ -90,17 +90,19 @@ class Pointcloud_Seg:
         self.min_p_v = 40 # 40 80 140   # minimum number of points to consider a blob as a valve    //PARAM
 
         # self.train_path = "RUNS/4_128_11_c9" # path to train
-        self.train_path = "../trained_models/12" # path to train
+        # self.train_path = "../trained_models/12" # path to train
+        self.train_path = "../trained_models/overfit"
         self.model_path = os.path.join(self.train_path, "model.ckpt")         # path to model         //PARAM
         self.path_cls =  os.path.join(self.train_path, "cls.txt")             # path to clases info   //PARAM
         self.classes, self.labels, self.label2color = indoor3d_util.get_info_classes(self.path_cls) # get classes info
+        robot_name = "sparus2"
 
         self.loop = 0
 
         self.out = True
         self.print = True
         self.time = True
-        self.path = rospy.get_param('/girona500/slamon/working_path', "../out")
+        self.path = rospy.get_param(f'/{robot_name}/slamon/working_path', "../out")
         self.path_out = os.path.join(self.path, "pipes")
         self.path_graph = os.path.join(self.path, "keyframes_poses.txt")
 
@@ -113,21 +115,21 @@ class Pointcloud_Seg:
 
 
         # set subscribers
-        pc_sub = message_filters.Subscriber('/girona500/map_slamon/keycloud', PointCloud2)         # //PARAM
-        odom_sub = message_filters.Subscriber('/girona500/map_slamon/robot_map', Odometry)      # //PARAM
+        pc_sub = message_filters.Subscriber(f'/{robot_name}/slamon_map/keycloud', PointCloud2)           # //PARAM
+        odom_sub = message_filters.Subscriber(f'/{robot_name}/slamon_map/robot_map', Odometry)      # //PARAM
 
         ts_pc_odom = message_filters.ApproximateTimeSynchronizer([pc_sub, odom_sub], queue_size=10, slop=0.001)
         ts_pc_odom.registerCallback(self.cb_pc)
 
-        loop_sub = message_filters.Subscriber('/girona500/map_slamon/loop_closure_num', Int32)
+        loop_sub = message_filters.Subscriber(f'/{robot_name}/slamon_map/loop_closure_num', Int32)
         loop_sub.registerCallback(self.cb_loop)
 
         # Set class image publishers
-        self.pub_pc_base = rospy.Publisher("/girona500/map_slamon/points2_base", PointCloud2, queue_size=4)
-        self.pub_pc_seg = rospy.Publisher("/girona500/map_slamon/points2_seg", PointCloud2, queue_size=4)
-        self.pub_pc_inst = rospy.Publisher("/girona500/map_slamon/points2_inst", PointCloud2, queue_size=4)
-        self.pub_pc_info = rospy.Publisher("/girona500/map_slamon/points2_info", PointCloud2, queue_size=4)
-        self.pub_pc_info_world = rospy.Publisher("/girona500/map_slamon/points2_info_world", PointCloud2, queue_size=4)
+        self.pub_pc_base = rospy.Publisher(f"/{robot_name}/slamon_map/points2_base", PointCloud2, queue_size=4)
+        self.pub_pc_seg = rospy.Publisher(f"/{robot_name}/slamon_map/points2_seg", PointCloud2, queue_size=4)
+        self.pub_pc_inst = rospy.Publisher(f"/{robot_name}/slamon_map/points2_inst", PointCloud2, queue_size=4)
+        self.pub_pc_info = rospy.Publisher(f"/{robot_name}/slamon_map/points2_info", PointCloud2, queue_size=4)
+        self.pub_pc_info_world = rospy.Publisher(f"/{robot_name}/slamon_map/points2_info_world", PointCloud2, queue_size=4)
 
         # Set segmentation timer
 
@@ -202,6 +204,7 @@ class Pointcloud_Seg:
         #     return
 
         pc_np = self.pc2array(pc)
+        print("[run]: " + str(pc_np.shape[0]))
         if pc_np.shape[0] < 2000:               # return if points < thr   //PARAM
             rospy.loginfo('[%s]: Not enough input points', self.name)
             return
@@ -457,6 +460,8 @@ class Pointcloud_Seg:
     def pc2array(self, ros_pc):
         gen = pc2.read_points(ros_pc, skip_nans=True)   # ROS pointcloud into generator
         pc_np = np.array(list(gen))                     # generator to list to numpy
+
+        print("[pc2array:] " + str(pc_np.shape[0]))
 
         if pc_np.size > 0:                              # if there are points
 
